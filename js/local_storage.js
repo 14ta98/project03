0c3fa84c7468132ad99faf299be312860fa43c5b
14ta98 <mokekedasho@icloud.com>
2015-03-09 23:26:19 +0900

    微調整

--- js/local_storage.js
+++ js/local_storage.js
@@ -25,13 +25,13 @@ function loaded() {
   $(".todoCtrl").click(
     // コールバックとしてメソッドを引数にわたす
     function() {
-      saveText();
+      saveText(if(checkText(text.val())) { /* ローカルストレージに保存する処理 */ });
       showText();
     });
 }
 
 // 入力された内容をローカルストレージに保存する
-function saveText() {
+function saveText(if(checkText(text.val())) { /* ローカルストレージに保存する処理 */ }) {
   // 時刻をキーにして入力されたテキストを保存する
   var text = $(".taskText");
   var time = new Date();
@@ -53,4 +53,40 @@ function showText() {
     html.push("<p>" + value + "</p>");
   }
   list.append(html.join(''));
+}
+
+var val = escapeText(text.val());
+if(checkText(val)) {
+  localStorage.setItem(time, escape(val));
+  // テキストボックスを空にする
+  text.val("");
+}
+
+// 文字をエスケープする
+function escapeText(text) {
+  return $("<div>").text(text).html();
+}
+
+// 入力チェックを行う
+function checkText(text) {
+  // 文字数が0または20以上は不可
+  if (0 === text.length || 20 < text.length) {
+    alert("文字数は1〜20字にしてください");
+    return false;
+  }
+
+  // すでに入力された値があれば不可
+  var length = localStorage.length;
+  for (var i = 0; i < length; i++) {
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+    // 内容が一致するものがあるか比較
+    if (text === value) {
+      alert("同じ内容は避けてください");
+      return false;
+    }
+  }
+
+  // すべてのチェックを通過できれば可
+  return true;
 }
